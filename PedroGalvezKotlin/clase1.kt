ce1f1754ff2e1e784024e4a9ed34bfd8b1bf427f
Pedro Galvez <37886993+pgalvez9@users.noreply.github.com>
2021-06-28 20:57:19 -0500

    Update clase1.kt

--- PedroGalvezKotlin/clase1.kt
+++ PedroGalvezKotlin/clase1.kt
@@ -1,5 +1,35 @@
 fun main() {
-    print("Hola mundo")
-    println("");
-	println("soy Pedro Galvez")
+    var nombre = "Pedro"
+    println(nombre)
+    val apellido = "Galvez"
+    println(apellido)
+    
+    nombre = "Javier"
+    
+    println(nombre)
+    println(apellido)
+    
+    var saludo = "Hola soy $nombre $apellido"
+    println(saludo)
+    
+    val text = """
+    	|linea uno
+        |linea dos
+        |linea tres
+        """.trimMargin();
+    
+    println(text)
+    
+    var str = "Kotlin string alfa"
+    var substr = ""
+    
+    substr = str.substring(0..5)
+    println("substring $substr")
+    
+    substr = str.substring(14..17)
+    println("alfa $substr")
+    
+    substr = str.substring(14..17)
+    println("alfa $substr")
+    
 }
